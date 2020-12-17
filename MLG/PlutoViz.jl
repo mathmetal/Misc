b9e317cd4df9231bd4238465d48151904a3a5734
igloo <4429609+marceline@users.noreply.github.com>
2020-12-17 16:27:37 +0530

    Gadfly done

--- MLG/PlutoViz.jl
+++ MLG/PlutoViz.jl
@@ -17,12 +17,31 @@ data=File("FluxTrain")|>DataFrame
 #select!(data,Not(:Column1))
 end
 
+# ╔═╡ 87118aa0-4005-11eb-074a-951a3d43a444
+plot(data,y="error(ms)")
+
+# ╔═╡ bbb04e40-4005-11eb-0a5e-af47afd2c872
+plot(data,x=:Column1,y="error(ms)",Geom.line,color=[colorant"red"])
+
 # ╔═╡ cb220b46-3ed6-11eb-0e87-6f9fb238cc8c
-plot(layer(data,y="error(ms)",x=:Column1,Geom.line,color=[colorant"red"]),
-	 layer(data,y=:Lx,color=[colorant"red",colorant"gold"]),
-	 layer(data,y=:Ly,Geom.line,color=[colorant"green",colorant"blue"],))
+begin
+	err=layer(data,y="error(ms)",x=:Column1,Geom.line,color=[colorant"red"])
+	left=layer(data,x=:Column1,y=:Ly,color=[colorant"silver",colorant"gold"])
+	right=layer(data,y=:Ry,Geom.line,color=[colorant"green",colorant"blue"])
+	plot(err,left,right,Guide.title("Everything Juxtaposed"))
+end
+
+# ╔═╡ 0839f3a0-4002-11eb-2d2b-2b8611a991a2
+vstack(map(plot,[err,left]))
+
+# ╔═╡ bf46a650-4003-11eb-0499-c7e69cb45f96
+hstack(map(plot,[err,left]))
 
 # ╔═╡ Cell order:
 # ╠═61fd28c2-3ed0-11eb-083a-05e43d8bae12
 # ╠═95b71286-3ed0-11eb-16a1-c1f5f0281a23
+# ╠═87118aa0-4005-11eb-074a-951a3d43a444
+# ╠═bbb04e40-4005-11eb-0a5e-af47afd2c872
 # ╠═cb220b46-3ed6-11eb-0e87-6f9fb238cc8c
+# ╠═0839f3a0-4002-11eb-2d2b-2b8611a991a2
+# ╠═bf46a650-4003-11eb-0499-c7e69cb45f96
